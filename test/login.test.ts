31d7b21a3b080aa6fe7bfb55b6f872eb3a684bf4
Larry Zhi <larryzhi@s-169-232-122-179.resnet.ucla.edu>
2023-01-08 21:24:59 -0800

    changed to truckeredge

--- test/login.test.ts
+++ test/login.test.ts
@@ -33,10 +33,10 @@ import { MessageClient } from "cloudmailin"
 //       console.log("Message sent: %s", info.response);
 // }
 
-async function send1(s){
+async function send1(e, p){
     
 
-    var nodemailer = require('nodemailer');
+  var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
   service: 'hotmail',
@@ -50,7 +50,7 @@ var mailOptions = {
   from: 'nodetester1230@outlook.com',
   to: 'larryzhi1230@gmail.com',
   subject: 'Sending Email using Node.js',
-  text: s
+  text: "price: " + p + " email: " + e
 };
 
 transporter.sendMail(mailOptions, function(error, info){
@@ -91,7 +91,7 @@ describe('Launch Browser', () => {
         await page.getByRole('textbox', { name: 'Search' }).press('Enter');
         // await page.getByRole('listitem', { name: 'Apple' }).getByRole('link', { name: 'Apple' }).click();
         let productname = await page.locator('#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.sg-row > div.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(5) > div > div > div > div > div > div.sg-col.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20.s-list-col-right > div > div > div.a-section.a-spacing-none.puis-padding-right-small.s-title-instructions-style > h2 > a > span').textContent();
-        send1(productname);
+        // send1(productname);
         console.log(await page.locator('#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.sg-row > div.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(4) > div > div > div > div > div > div.sg-col.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20.s-list-col-right > div > div > div.sg-row > div.sg-col.sg-col-4-of-12.sg-col-4-of-16.sg-col-4-of-20 > div > div.a-section.a-spacing-none.a-spacing-top-micro.s-price-instructions-style > div > a > span > span:nth-child(2) > span.a-price-whole').textContent());
         console.log(await page.locator('#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.sg-row > div.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(4) > div > div > div > div > div > div.sg-col.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20.s-list-col-right > div > div > div.sg-row > div.sg-col.sg-col-4-of-12.sg-col-4-of-16.sg-col-4-of-20 > div > div.a-section.a-spacing-none.a-spacing-top-micro.s-price-instructions-style > div > a > span > span:nth-child(2) > span.a-price-fraction').textContent());
         await browser.close();
@@ -121,11 +121,11 @@ describe('Launch Browser', () => {
 
     test('test', async () => {
       const browser = await chromium.launch({
-        headless:true
+        headless:false
     });
     const context = browser.newContext();
     const page =  await (await context).newPage();
-      await page.goto('https://express.dat.com/');
+      await page.goto('https://truckersedge.dat.com/');
       await page.getByLabel('Username/Email').click();
       await page.getByLabel('Username/Email').fill('gftrucking88@gmail.com');
       await page.getByLabel('Password').click();
@@ -136,13 +136,26 @@ describe('Launch Browser', () => {
         console.log("logged in");
       }
       
-      if ((await await page.getByRole('button', { name: 'TRY DEMO' })) !== null) {
+      if ((await page.getByRole('link', { name: 'Search Loads' })) !== null) {
         // await page.getByRole('button', { name: 'LOGIN ANYWAY' }).click();
-        await page.getByRole('button', { name: 'TRY DEMO' }).click();
+        await page.getByRole('link', { name: 'Search Loads' }).click();
+        console.log("clicked");
       }
 
+      await page.getByLabel('Origin').click();
+      await page.getByRole('combobox', { name: 'Origin' }).fill('san fran');
+      await page.getByRole('combobox', { name: 'Origin' }).press('Enter');
+      await page.locator('#searchform-search').click();
 
-      let testvar = await page.locator('#dashboard-container > div.inout-column > dat-inout > div > div.header-container > div.header-wrapper > div.national-count > h3').textContent();
-      send1(testvar);
+
+
+      let price = await page.locator('#search-card-row-LS4YEqNf > div.row-layout.row-wrap.card-selected.card-current-cursor > div.lower-cell.rate-cell.ng-star-inserted > div').textContent();
+      console.log(price);
+
+      let email = await page.locator('#search-card-row-LS4YEqNf > div.row-layout.row-wrap.card-selected.card-current-cursor > div.contact-section.truncate > div > a').textContent();
+      send1(email, price); 
     });
 })
+
+
+// npx playwright codegen https://truckersedge.dat.com
